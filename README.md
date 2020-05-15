28647a4e892fbdbde7888596459fadfd11e0f28b
dstasio <davide.stasio@gmail.com>
2020-05-15 23:56:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hello_u
+
+Developed with Unreal Engine 4
---
+++ Source/hello_u/hello_uGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "hello_uGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELLO_U_API Ahello_uGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
